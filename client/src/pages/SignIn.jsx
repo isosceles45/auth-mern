1a48652bda541a5db49a41b42873de857c08e63d
isosceles45 <atharvasardal06@gmail.com>
2023-10-29 12:23:58 +0530

    Persist Storage

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,56 +1,47 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import {
   signInStart,
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
-const SignIn = () => {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
-
-  const handleChange = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.id]: event.target.value,
-    });
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
       dispatch(signInStart());
-      const response = await fetch("/api/auth/signin", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
-      const data = await response.json();
-
+      const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailure(data));
         return;
       }
-
       dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
       dispatch(signInFailure(error));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-6">Sign In</h1>
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="email"
@@ -67,23 +58,21 @@ const SignIn = () => {
           onChange={handleChange}
         />
         <button
-          disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg font-medium hover:opacity-90 disabled:opacity-50"
+        disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
           {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
-      <div className="flex justify-between py-3 px-1">
-        <p>Dont have an account?</p>
+      <div className="flex gap-2 mt-5 justify-between">
+        <p>Dont Have an account?</p>
         <Link to="/sign-up">
-          <span className="text-blue-600">Sign Up</span>
+          <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className="px-1 font-normal text-red-600">
-        {error ? error || "Something went wrong. Try again!" : ""}
+      <p className="text-red-700 mt-5">
+        {error ? error.message || "Something went wrong!" : ""}
       </p>
     </div>
   );
-};
-
-export default SignIn;
+}
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit"; // 1. import configureStore
-import userReducer from "./user/userSlice"; // 2. import userReducer
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export default configureStore({
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
   // 3. export configureStore
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
